8d39b7e2c83a643aed3853eac166f9e396932fc6
kodasom0302 <dasom589@gmail.com>
2024-04-04 11:16:41 +0900

    2024-04-04

--- src/main/java/com/javaex/service/MovieService.java
+++ src/main/java/com/javaex/service/MovieService.java
@@ -1,15 +1,9 @@
 package com.javaex.service;
 
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.javaex.dao.MovieDao;
 import com.javaex.vo.MovieVo;
